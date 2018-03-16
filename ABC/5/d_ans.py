7398f328b3675c677fbaadb2d3af84e4142762b4
itouri <hechima.itouri@gmail.com>
2018-03-16 21:23:51 +0900

    Complaten d_and.py

--- ABC/5/d_ans.py
+++ ABC/5/d_ans.py
@@ -1,5 +1,3 @@
-import functools
-
 N = int(input())
 D = [0]*N
 for i in range(N):
@@ -11,26 +9,32 @@ max_values = [0 for i in range(N*N+1)]
 
 taste = [[0 for i in range(N)] for j in range(N)]
 taste[0][0] = D[0][0]
+# 最初に縦と横の端一列の美味しさを求めてしまう
 for i in range(1, N):
     taste[0][i] = D[0][i] + taste[0][i-1]
     taste[i][0] = D[i][0] + taste[i-1][0]
 
 for i in range(1,N):
     for j in range(1,N):
+        # これだけで taste が全部求まるのすごいキレイ
         taste[i][j] = taste[i-1][j] + taste[i][j-1] - taste[i-1][j-1] + D[i][j]
-print(taste)
 
 for si in range(N):
     for sj in range(N):
         for ei in range(si, N):
             for ej in range(sj, N):
-                row = 0 if si == 0 else taste[ei][sj-1]
-                col = 0 if sj == 0 else taste[si-1][ej]
+                # 毎度のこと座標でわけがわからなくなる
+                # 解説でいう緑
+                row = 0 if sj == 0 else taste[ei][sj-1]
+                # 解説でいう紫
+                col = 0 if si == 0 else taste[si-1][ej]
+                # 解説でいう青
                 mid = 0 if si == 0 or sj == 0 else taste[si-1][sj-1] 
 
                 v = mid - col - row + taste[ei][ej]
-                size = abs(si-ei+1)*abs(sj-ej+1)
-                print(si, sj, ei, ej, size, v)
+                size = abs(ei-si+1)*abs(ej-sj+1)
+                # print(si, sj, ei, ej, size, v, mid , col , row, taste[ei][ej])
+                # print(si, sj, ei, ej, size, v)
                 max_values[size] = max(max_values[size], v)
 
 for p in P:

13bda4414751c8257679aacb38c58e4d2e6ee7bc
itouri <hechima.itouri@gmail.com>
2018-03-17 15:42:02 +0900

    torima commit

--- ABC/87/d_ans.py
+++ ABC/87/d_ans.py
@@ -1,8 +1,28 @@
 N, M = list(map(int, input().split()))
-LRD = [list(map(int, input().split())) for i in range(M) ]
-
 G = [[] for _ in range(N+1)]
-for lrd in LRD:
-    l, r, d = lrd
-    G[l][r] = d
-    G[r][l] = -d
+x = [None] * (N+1)
+
+def fail():
+    print("No")
+    exit()
+
+def dfs(s):
+    for t, d in G[s]:
+        if x[t] is None:
+            x[t] = x[s] + d
+            dfs(t)
+        else:
+            if x[t] != x[s] + d:
+                fail()
+
+for _ in range(M):
+    L, R, D = map(int, input().split())
+    G[L].append([R,  D])
+    G[R].append([L, -D])
+
+for i in range(N):
+    if x[i] is None:
+        x[i] = 0
+        dfs(i)
+
+print("Yes")

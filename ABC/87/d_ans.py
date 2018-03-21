1cef32c1ead3456e8be4a12b6874fe96d582d900
itouri <hechima.itouri@gmail.com>
2018-03-21 19:40:57 +0900

    Implementd abc 87 ans

--- ABC/87/d_ans.py
+++ ABC/87/d_ans.py
@@ -1,12 +1,17 @@
-N, M = list(map(int, input().split()))
-G = [[] for _ in range(N+1)]
-x = [None] * (N+1)
+# コレがないせいでずっと RE になってた
+import sys
+sys.setrecursionlimit(200002)
+
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+x = [None] * N
 
 def fail():
     print("No")
     exit()
 
 def dfs(s):
+    global x, G
     for t, d in G[s]:
         if x[t] is None:
             x[t] = x[s] + d
@@ -17,8 +22,8 @@ def dfs(s):
 
 for _ in range(M):
     L, R, D = map(int, input().split())
-    G[L].append([R,  D])
-    G[R].append([L, -D])
+    G[L-1].append([R-1,  D])
+    G[R-1].append([L-1, -D])
 
 for i in range(N):
     if x[i] is None:

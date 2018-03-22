195f19af43c9ae5888c40264fe7c796fcabd40e5
itouri <hechima.itouri@gmail.com>
2018-03-22 14:23:46 +0900

    Modified abc 86 c to more shorter

--- ABC/86/c.py
+++ ABC/86/c.py
@@ -7,10 +7,8 @@ for i in range(N):
     t1, x1, y1 = txys[i]
     t2, x2, y2 = txys[i+1]
     dt = t2 - t1
-    tIsEven = dt % 2 == 0
     dist = abs(x1 - x2) + abs(y1 - y2)
-    xyIsEven = dist % 2 == 0
-    if tIsEven != xyIsEven or dist > t2:
+    if dt % 2 != dist % 2 or dist > dt:
         print("No")
         exit()
 print("Yes")
